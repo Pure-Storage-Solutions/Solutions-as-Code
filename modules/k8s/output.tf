17f90cd8697186152fab463d203cd03f5ece22d5
Unnikrishnan R <unnikrishnan.chaithanya@gmail.com>
2025-03-11 22:09:01 +0530

    added code

--- modules/k8s/output.tf
+++ modules/k8s/output.tf
@@ -4,3 +4,7 @@ output "vm_ip" {
 output "vm_name" {
   value = "${module.vm.vm_name}"
 }
+
+output "new_ip_address" {
+  value = "${module.vm.*.clone.0.customize.0.network_interface.0.ipv4_address}"
+}
