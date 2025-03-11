47287f8b5928cf7d76cb786c6fde46f64442ffac
Unnikrishnan R <unnikrishnan.chaithanya@gmail.com>
2025-03-11 22:03:49 +0530

    added code

--- modules/basemodule-px/output.tf
+++ modules/basemodule-px/output.tf
@@ -4,3 +4,6 @@ output "vm_ip" {
 output "vm_name" {
   value = vsphere_virtual_machine.vm.*.name
 }
+output "new_ip_address" {
+  value = vsphere_virtual_machine.vm.*.clone.0.customize.0.network_interface.0.ipv4_address
+}
